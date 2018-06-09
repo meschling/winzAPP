ff6a557041c2080419de5deb73da2e1e0a641389
Thorge Neelsen <thorge@roamsys.com>
2018-06-09 18:15:04 +0200

    Changed Project Name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to vinzApp!');
+    expect(page.getParagraphText()).toEqual('Welcome to winzApp!');
   });
 });
